b1ada9f0b2e454c974fdd02fc438cbeeab6e1985
vidalkaique <dosreiskaiquevidal@gmail.com>
2025-08-09 18:41:48 -0300

    Change header title color to black

--- client/src/components/hero-section.tsx
+++ client/src/components/hero-section.tsx
@@ -1,7 +1,7 @@
 export default function HeroSection() {
   return (
     <section className="text-center px-4 pt-8 pb-4">
-      <h1 className="text-2xl font-bold text-red-600 mb-8">EXAME DE FAIXA<br />2025</h1>
+      <h1 className="text-2xl font-bold text-black mb-8">EXAME DE FAIXA<br />2025</h1>
     </section>
   );
 }
